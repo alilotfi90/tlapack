a38862021487239756aa1edd9a7c2be98f155fe9
alilotfi90 <ali.lotfi@colorado.edu>
2022-10-29 18:20:57 -0600

    working on finishing ux=l recursive

--- include/tlapack/lapack/getri_uxlinv_recursive.hpp
+++ include/tlapack/lapack/getri_uxlinv_recursive.hpp
@@ -56,70 +56,26 @@ int getri_uxlinv_recursive( matrix_t& A){
     if(n==1){
         // 1-by-1 non-invertible case
         if(A(0,0)==T(0))
-            return -1;
+            return 1;
         // inverse of 1-by-1 case
         A(0,0) = T(1)/A(0,0); 
     }
     else{
         
-        // n>1 case
-        // breaking the matrix into four blocks
         idx_t k0 = n/2;
+        
         auto X00 = tlapack::slice(A,tlapack::range<idx_t>(0,k0),tlapack::range<idx_t>(0,k0));
         auto X01 = tlapack::slice(A,tlapack::range<idx_t>(0,k0),tlapack::range<idx_t>(k0,n));
         auto X10 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(0,k0));
-        auto X11 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(k0,n));
-        
-        
-        trsm(Side::Left,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(1),X11,L10);
-            
+        auto X11 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(k0,n));    
     
-
-
-
-        auto A00 = tlapack::slice(A,tlapack::range<idx_t>(0,k0),tlapack::range<idx_t>(0,k0));
-        auto U01 = tlapack::slice(A,tlapack::range<idx_t>(0,k0),tlapack::range<idx_t>(k0,n));
-        auto L10 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(0,k0));
-        auto A11 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(k0,n));
-        
-        
-        
-        
         //step1:
-        // the following three lines calculates - U_11^{-1} (L_11^{-1} (L_10^{-1} L_00^{-1})) 
-        // L00 is the subdiagonal of A_00 with 1 on the diagonal
-        // L11 is the subdiagonal of A_11 with 1 on the diagonal
-        // U11 is the superdiagonal and diagonal of A_11
-        
-        // compute L10 L00^{-1}
-        trsm(Side::Right,Uplo::Lower,Op::NoTrans,Diag::Unit,T(1),A00,L10);
+        // X10 <--- X11^{-1} X10
+        trsm(Side::Left,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(-1),X11,X10);
 
-        // compute L11^{-1} L10
-        trsm(Side::Left,Uplo::Lower,Op::NoTrans,Diag::Unit,T(1),A11,L10);
-
-        // compute U11^{-1} L10
-        trsm(Side::Left,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(-1),A11,L10);
-        
-        //step2: compute U00^{-1} U01
-        // U00 is the diagonal and superdiagonal of A00  
-        trsm(Side::Left,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(1),A00,U01);
-    
-        //step3: recursive call on A00
-        getri_uxlinv_recursive(A00);
-
-        //step4:
-        // A00 <---- A00 - (U01 * L10)
-        gemm(Op::NoTrans,Op::NoTrans,T(-1),U01,L10,T(1),A00);
-    
-
-        //step5: the two following computes -(U01 U11^{-1}) L_11^{-1}
-        trsm(Side::Right,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(1),A11,U01);
-
-        trsm(Side::Right,Uplo::Lower,Op::NoTrans,Diag::Unit,T(-1),A11,U01);
-        
-        //step6:recursive call on A11
-        getri_uxlinv_recursive(A11);
-    
+        //step2:
+        // recursive call
+        getri_uxlinv_recursive(X11);
     
     }
         
@@ -133,3 +89,66 @@ int getri_uxlinv_recursive( matrix_t& A){
 
 
 
+
+
+// n>1 case
+        // breaking the matrix into four blocks
+        // trtri_recursive(Uplo::Lower, Diag::Unit, A);
+        // idx_t k0 = n/2;
+        
+        // auto X00 = tlapack::slice(A,tlapack::range<idx_t>(0,k0),tlapack::range<idx_t>(0,k0));
+        // auto X01 = tlapack::slice(A,tlapack::range<idx_t>(0,k0),tlapack::range<idx_t>(k0,n));
+        // auto X10 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(0,k0));
+        // auto X11 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(k0,n));
+        
+        
+        
+        // trsm(Side::Left,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(1),X11,X10);
+            
+        // // A00 <---- A00 - (U01 * L10)
+        // gemm(Op::NoTrans,Op::NoTrans,T(-1),U01,L10,T(1),A00);
+
+
+
+        // auto A00 = tlapack::slice(A,tlapack::range<idx_t>(0,k0),tlapack::range<idx_t>(0,k0));
+        // auto U01 = tlapack::slice(A,tlapack::range<idx_t>(0,k0),tlapack::range<idx_t>(k0,n));
+        // auto L10 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(0,k0));
+        // auto A11 = tlapack::slice(A,tlapack::range<idx_t>(k0,n),tlapack::range<idx_t>(k0,n));
+        
+        
+        
+        
+        // //step1:
+        // // the following three lines calculates - U_11^{-1} (L_11^{-1} (L_10^{-1} L_00^{-1})) 
+        // // L00 is the subdiagonal of A_00 with 1 on the diagonal
+        // // L11 is the subdiagonal of A_11 with 1 on the diagonal
+        // // U11 is the superdiagonal and diagonal of A_11
+        
+        // // compute L10 L00^{-1}
+        // trsm(Side::Right,Uplo::Lower,Op::NoTrans,Diag::Unit,T(1),A00,L10);
+
+        // // compute L11^{-1} L10
+        // trsm(Side::Left,Uplo::Lower,Op::NoTrans,Diag::Unit,T(1),A11,L10);
+
+        // // compute U11^{-1} L10
+        // trsm(Side::Left,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(-1),A11,L10);
+        
+        // //step2: compute U00^{-1} U01
+        // // U00 is the diagonal and superdiagonal of A00  
+        // trsm(Side::Left,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(1),A00,U01);
+    
+        // //step3: recursive call on A00
+        // getri_uxlinv_recursive(A00);
+
+        // //step4:
+        // // A00 <---- A00 - (U01 * L10)
+        // gemm(Op::NoTrans,Op::NoTrans,T(-1),U01,L10,T(1),A00);
+    
+
+        // //step5: the two following computes -(U01 U11^{-1}) L_11^{-1}
+        // trsm(Side::Right,Uplo::Upper,Op::NoTrans,Diag::NonUnit,T(1),A11,U01);
+
+        // trsm(Side::Right,Uplo::Lower,Op::NoTrans,Diag::Unit,T(-1),A11,U01);
+        
+        // //step6:recursive call on A11
+        // getri_uxlinv_recursive(A11);
